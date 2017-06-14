391b6f282e412858694641037c7ba687ecd47c37
Anthony Alaribe <anthonyalaribe@gmail.com>
2017-06-14 14:12:37 +0100

    UI improvements on the promos list page

--- public/merchant/containers/promos.js
+++ public/merchant/containers/promos.js
@@ -2,47 +2,58 @@ import m from "mithril";
 import {Promos} from "../models/promos.js";
 
 var PromosTable = {
-    
+
   view:function(){
     var p = Promos.AllPromos
     return (
       <section>
-        <div class="pa2 bg-white shadow-m2 mt3 cf">
-            <div class="tc bb b--light-gray">
-              <h3>All Promos</h3>
+        <div class="ph4 pv4 bg-white shadow-m2 ">
+          <div class="">
+            <span  class="fw6 f3">All Promos </span>
+            <a href="/promos/new" class="fr ph3 pv2 bg-navy white-90 grow pointer no-underline shadow-4" oncreate={m.route.link}>New Promo</a>
             </div>
-            <div class="ba pa2 b--light-gray" style="overflow: auto">
+        </div>
+        <div class="pa3 pa4-ns bg-white shadow-m2 mt3 cf">
+            <div class="" style="overflow: auto">
               <table class="f6 w-100 mw8 center" cellspacing="0">
-                  <thead class="pa2">
-                      <th class="fw6 bb b--black-20 tl pb3 pr3 bg-white">S/N</th>
-                      <th class="fw6 bb b--black-20 tl pb3 pr3 bg-white">Name</th>
-                      <th class="fw6 bb b--black-20 tl pb3 pr3 bg-white">Category</th>
-                      <th class="fw6 bb b--black-20 tl pb3 pr3 bg-white">OldPrice</th>
-                      <th class="fw6 bb b--black-20 tl pb3 pr3 bg-white">NewPrice</th>
-                      <th class="fw6 bb b--black-20 tl pb3 pr3 bg-white">Description</th>
-                      <th class="fw6 bb b--black-20 tl pb3 pr3 bg-white">StartDate</th>
-                      <th class="fw6 bb b--black-20 tl pb3 pr3 bg-white">EndDate</th>
-                      <th class="fw6 bb b--black-20 tl pb3 pr3 bg-white">Edit</th>
+                  <thead class="pa2 ">
+                      <th class="fw6 bb b--black-20 tl pb3 pr3 bg-white tc">S/N</th>
+                      <th class="fw6 bb b--black-20 tl pb3 pr3 bg-white tc">Name</th>
+                      <th class="fw6 bb b--black-20 tl pb3 pr3 bg-white tc">Category</th>
+                      <th class="fw6 bb b--black-20 tl pb3 pr3 bg-white tc">Price</th>
+                      <th class="fw6 bb b--black-20 tl pb3 pr3 bg-white tc">Duration</th>
+                      <th class="fw6 bb b--black-20 tl pb3 pr3 bg-white tc">Actions</th>
                   </thead>
                   <tbody class="lh-copy">
                       {p.map(function(promo) {
                         return (<tr class="">
-                          <td class="pv3 pr3 bb b--black-20">{promo.ID}</td>
-                          <td class="pv3 pr3 bb b--black-20">{promo.Name}</td>
-                          <td class="pv3 pr3 bb b--black-20">{promo.Category}</td>
-                          <td class="pv3 pr3 bb b--black-20">{promo.OldPrice}</td>
-                          <td class="pv3 pr3 bb b--black-20">{promo.NewPrice}</td>
-                          <td class="pv3 pr3 bb b--black-20">{promo.Description}</td>
-                          <td class="pv3 pr3 bb b--black-20">{promo.StartDate}</td>
-                          <td class="pv3 pr3 bb b--black-20">{promo.EndDate}</td>
-                          <td class="pv3 pr3 bb b--black-20"><button type="button" class="ph1 pv2 ba b--white shadow-4 bg-red white">Del</button></td>
+                          <td class="pv3 pr3 bb b--black-20 tc">{promo.ID}</td>
+                          <td class="pv3 pr3 bb b--black-20 tc">{promo.Name}</td>
+                          <td class="pv3 pr3 bb b--black-20 tc">{promo.Category}</td>
+                          <td class="pv3 pr3 bb b--black-20 tc">
+                            <span class="db">{promo.NewPrice}</span>
+                            <span class="db">-</span>
+                            <span class="db strike">{promo.OldPrice}</span>
+                          </td>
+                          <td class="pv3 pr3 bb b--black-20 tc">
+                            <span class="db">{promo.StartDate}</span>
+                            <span class="db">to</span>
+                            <span class="db ">{promo.EndDate}</span>
+                          </td>
+                          <td class="pv3 pr3 bb b--black-20 tc">
+                            <a href="/promos/new" class="ph2 pv1 bg-navy white-90 grow pointer no-underline ma1 shadow-4" oncreate={m.route.link}>Edit</a>
+
+                            <a href="/promos/new" class="ph2 pv1 bg-navy white-90 grow pointer no-underline ma1 shadow-4" oncreate={m.route.link}>Delete</a>
+
+                          </td>
                         </tr>);
                       })}
                   </tbody>
               </table>
             </div>
-            <div class="mt2 cf pa2">
-              <a href="/promos/new" class="fr ph3 pv2 bg-white blue ba b--blue bw1 shadow-3 pointer no-underline" oncreate={m.route.link}>Add Promo</a>
+            <div class="mt4 cf pa2 pv3">
+              <a href="/promos/new" class="fr ph3 pv2 bg-navy white-90 grow pointer no-underline mh1 shadow-4" oncreate={m.route.link}>Previous</a>
+              <a href="/promos/new" class="fr ph3 pv2 bg-navy white-90 grow pointer no-underline mh1 shadow-4" oncreate={m.route.link}>Next</a>
             </div>
         </div>
       </section>

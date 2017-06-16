4e91bc21b1d28972f1e730abe988d53516fbb3d2
spankie <odohi.david@gmail.com>
2017-06-16 23:30:57 +0100

    new branch form

--- public/merchant/models/branches.js
+++ public/merchant/models/branches.js
@@ -1,6 +1,10 @@
-export var branches = {
+export var br = {
+    NewBranch:{},
     AllBranches:{},
     GetAllBranches : function() {
         // TODO :: GET all my branches
+    },
+    SaveNewBranch: function() {
+        console.log("new branch: ", this.NewBranch);
     }
 }

d1d8e4c0c4f783828b54bc50a4ec91337aaa2223
spankie <odohi.david@gmail.com>
2017-06-24 15:34:13 +0100

    new location form validations

--- public/admin/containers/locations.js
+++ public/admin/containers/locations.js
@@ -2,6 +2,29 @@ import m from 'mithril';
 import {LocationModel} from '../models/locationmodel.js';
 
 var Locations = {
+    VerifyNewLocation:() => {
+        if(!LocationModel.NewLocation.country || !LocationModel.NewLocation.city || !LocationModel.NewLocation.neighbourhood) {
+            Locations.state.NewLocationMessage = "";
+            Locations.state.NewLocationError = "Please fill out all required fields.";
+            return;
+        }
+        Locations.state.Loader = true;
+        LocationModel.AddLocation().then(()=>{
+            Locations.state.NewLocationError = "";
+            Locations.state.NewLocationMessage = "New Location Added.";
+            Locations.state.Loader = false;
+        }).catch((error) => {
+            console.error("Add location error: ", error);
+            Locations.state.NewLocationMessage = "";
+            Locations.state.NewLocationError = "Could not add this location. Maybe it already exists.";
+            Locations.state.Loader = false;
+        })
+    },
+    state: {
+        Loader: false,
+        NewLocationError: "",
+        NewLocationMessage: ""
+    },
     view: (vnode) => {
         return (
             <section>
@@ -9,6 +32,8 @@ var Locations = {
                     <h3>Add Locations</h3>
                 </div>
                 <div class="pa3 bg-white shadow-m2 mt3 cf">
+                    {Locations.state.NewLocationError?m("p.pa1.bg-red.mv0.white.w5.br1.tc", Locations.state.NewLocationError):""}
+                    {Locations.state.NewLocationMessage?m("p.pa1.bg-navy.mv0.white.w5.br1.tc", Locations.state.NewLocationMessage):""}
                     <div class="">
                         {/*<label class="f4 mv2">Country: </label><br/>*/}
                         <input type="text" placeholder="Country" class="pa2 ba b--gray w5 bw1 mv1"
@@ -25,10 +50,10 @@ var Locations = {
                         oninput={m.withAttr("value", function(value){
                                 LocationModel.NewLocation.neighbourhood = value;
                             })} /><br/>
-                        <button type="button" class="ba b--navy bg-navy pa2 white shadow-3 pointer mv1" onclick={() => {
+                        <button type="button" class="ba b--navy bg-navy pv2 ph4 white shadow-3 pointer mv1" onclick={() => {
                                 console.log("Add Location clicked");
-                                LocationModel.AddLocation();
-                            }}>Add Location</button>
+                                Locations.VerifyNewLocation();
+                            }}>{Locations.state.Loader?m(".loader.mv0"):"Add Location"}</button>
                     </div>
                 </div>
                 <section class="pa3 bg-white shadow-m2 mt3 cf" >
--- public/admin/models/locationmodel.js
+++ public/admin/models/locationmodel.js
@@ -3,14 +3,12 @@ import m from 'mithril';
 export var LocationModel = {
     NewLocation: {},
     AddLocation: () => {
-        m.request({
+        return m.request({
             method: "POST",
             url: "/api/admins/locations/neighbourhood",
             data: LocationModel.NewLocation
         }).then((response) => {
             console.log("Add location response: ", response);
-        }).catch((error) => {
-            console.error("Add location error: ", error);
         })
     }
 }

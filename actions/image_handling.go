d9da52a7a17eb7af57df90fc5facdeb3fc22a7ad
Anthony Alaribe <anthonyalaribe@gmail.com>
2017-07-16 07:39:47 +0100

    building for dokku without vips or need for cgo

--- actions/image_handling.go
+++ actions/image_handling.go
@@ -1,13 +1,16 @@
 package actions
 
 import (
+	"bytes"
 	"encoding/base64"
 	"fmt"
+	"image"
+	"image/jpeg"
 	"log"
 	"strings"
 
+	"github.com/disintegration/imaging"
 	"github.com/goamz/goamz/s3"
-	bimg "gopkg.in/h2non/bimg.v1"
 )
 
 //UploadBase64Image resizes image and puts it into s3 bucket
@@ -43,37 +46,35 @@ func UploadImageObjectToS3(bucket *s3.Bucket, b []byte, imagename string) (strin
 }
 
 //CompressAndBlurImageAndReturnBase64 Helper turns compresses and blurs image, then turns it to base64 string of less than 1kb
-func CompressAndBlurImageAndReturnBase64(image string) (string, error) {
-	byt, err := base64.StdEncoding.DecodeString(strings.Split(image, "base64,")[1])
-	opt := bimg.Options{
-		Width: 25,
-		GaussianBlur: bimg.GaussianBlur{
-			MinAmpl: 0.2,
-			Sigma:   3,
-		},
-	}
+func CompressAndBlurImageAndReturnBase64(img string) (string, error) {
+	byt, err := base64.StdEncoding.DecodeString(strings.Split(img, "base64,")[1])
 
-	newImage, err := bimg.Resize(byt, opt)
+	srcImage, _, _ := image.Decode(bytes.NewReader(byt))
+	dstImage25 := imaging.Resize(srcImage, 25, 0, imaging.CatmullRom)
+	buf := new(bytes.Buffer)
+	err = jpeg.Encode(buf, dstImage25, nil)
 	if err != nil {
 		log.Println(err)
 		return "", err
 	}
+
 	//bimg.Write("smaller.jpg", newImage)
 	return fmt.Sprintf("data:%s;base64,%s",
-		"image/jpeg", base64.StdEncoding.EncodeToString(newImage)), nil
+		"image/jpeg", base64.StdEncoding.EncodeToString(buf.Bytes())), nil
 }
 
 //ResizeToThumbnailSize resizes image to a 300px width
 func ResizeToThumbnailSize(byt []byte) ([]byte, error) {
-	opt := bimg.Options{
-		Width: 300,
-	}
 
-	newImage, err := bimg.Resize(byt, opt)
+	srcImage, _, _ := image.Decode(bytes.NewReader(byt))
+	dstImage25 := imaging.Resize(srcImage, 300, 0, imaging.CatmullRom)
+	buf := new(bytes.Buffer)
+	err := jpeg.Encode(buf, dstImage25, nil)
 	if err != nil {
 		log.Println(err)
-		return []byte{}, err
+		return byt, err
 	}
+
 	//bimg.Write("smaller.jpg", newImage)
-	return newImage, nil
+	return buf.Bytes(), nil
 }
